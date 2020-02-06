232a26725726371f26fcc3f356d1affa1b14d88a
Gaurav Labhane <labhanegaurav@gmail.com>
2020-02-06 17:03:23 +0530

    Updated solution

--- three.js
+++ three.js
@@ -21,25 +21,45 @@
 
 
 
-//Method 2
-var myFunc2 = function (value) {
-  console.log(value);
-};
-var i = 1;
-function method2() {
+// //Method 2
+// var myFunc2 = function (value) {
+//   console.log(value);
+// };
+// var i = 1;
+// function method2() {
 
-  if (i < 51) {
-    myFunc2(i);
-    i += 1;
-  }
-  if (i == 51) {
-    console.log("Bye");
-    clearInterval(id);
+//   if (i < 51) {
+//     myFunc2(i);
+//     i += 1;
+//   }
+//   if (i == 51) {
+//     console.log("Bye");
+//     clearInterval(id);
 
-  }
+//   }
+// }
+
+i = 0;
+function func(value, callback) {
+  setTimeout(function () {
+    if (value != 51) {
+      console.log(value);
+      return callback(null, value);
+
+    }
+    console.log('Bye');
+
+
+  }, 1000);
 }
 
-var id = setInterval(method2, 1000);
 
+function callback(err, y) {
+  y += 1;
+  if (y < 52) {
+    func(y, callback);
+  }
 
+}
 
+func(1, callback);
